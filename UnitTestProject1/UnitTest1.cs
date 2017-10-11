4c2cd50651d7eac941d536629823fcee7e8b7a47
Glen Turnbow <glenturnbow@gmail.com>
2017-10-11 15:13:36 -0600

    First Commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace UnitTestProject1
+{
+    public class FileRename
+    {
+        [Fact]
+        public void TestCheckNumberOfFiles()
+        {
+            //Test
+        }
+
+        [Fact]
+        public void TestChangeFileName()
+        {
+
+        }
+
+
+
+    }
+}
